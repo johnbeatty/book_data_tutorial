d0bca14840eb74184d15a57efe78b0a06da53cf0
John Beatty <john@thebeattys.org>
2018-09-25 17:24:32 -0400

    Adding Faker and seed data generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+2_000.times do |i|
+  Book.create!( title: Faker::Book.title, 
+    author: Faker::Book.author, 
+    publisher: Faker::Book.publisher,
+    category: Faker::Book.genre,
+    isbn: "#{Faker::Number.number(3)}-#{Faker::Number.number(1)}-#{Faker::Number.number(2)}-#{Faker::Number.number(6)}-#{Faker::Number.number(1)}",
+    dewey_decimal_number: "#{Faker::Number.number(3)}.#{Faker::Number.number(3)}",
+    binding: Faker::Number.between(0,1))
+  print '.' if i % 100 == 0
+end
